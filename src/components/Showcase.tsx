3979a734e80802e2527155ac7e83567ee7eaa689
Dz.dev <darwinbjordan@gmail.com>
2024-07-07 09:39:59 +0800

    chore: map images for showcase page

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,6 +1,32 @@
+import DesignOne from "../assets/Bodaq-blog-post-pics-1-1-3-1024x648.jpg";
+import DesignFour from "../assets/Bodaq-blog-post-pics-1-16-1024x648.jpg";
+import DesignThree from "../assets/Bodaq-blog-post-pics-1-2-3-1024x648.jpg";
+import DesignTwo from "../assets/Bodaq-blog-post-pics-1-9-1-1024x648.jpg";
 import "../style/clip.css";
 
 export default function Showcase() {
+  const imageAssets = [
+    {
+      productId: 1,
+      productImage: DesignOne,
+      productLabel: "Interior Design",
+    },
+    {
+      productId: 2,
+      productImage: DesignTwo,
+      productLabel: "Interior Design",
+    },
+    {
+      productId: 3,
+      productImage: DesignThree,
+      productLabel: "Interior Design",
+    },
+    {
+      productId: 4,
+      productImage: DesignFour,
+      productLabel: "Interior Design",
+    },
+  ];
   return (
     <section className="bg-[#F8F7EC] h-full cursor-sakura">
       <div className="container flex justify-between mr-auto ml-auto items-center pb-[4.3rem]">
@@ -10,26 +36,13 @@ export default function Showcase() {
           </span>
         </span>
         <div className="grid grid-cols-2 max-w-[70%] gap-4 mt-5">
-          <img
-            className="rounded-md block transition duration-[1.3s] ease-in-out hover:scale-[1.1]"
-            src="./src/assets/Bodaq-blog-post-pics-1-1-3-1024x648.jpg"
-            alt=""
-          />
-          <img
-            className="transition duration-[1.3s] ease-in-out hover:scale-[1.1] rounded-md block"
-            src="./src/assets/Bodaq-blog-post-pics-1-9-1-1024x648.jpg"
-            alt=""
-          />
-          <img
-            className="transition duration-[1.3s] ease-in-out hover:scale-[1.1] rounded-md block"
-            src="./src/assets/Bodaq-blog-post-pics-1-2-3-1024x648.jpg"
-            alt=""
-          />
-          <img
-            className="transition duration-[1.3s] ease-in-out hover:scale-[1.1] rounded-md block"
-            src="./src/assets/Bodaq-blog-post-pics-1-16-1024x648.jpg"
-            alt=""
-          />
+          {imageAssets.map((collection) => (
+            <img
+              className="rounded-md block transition duration-[1.3s] ease-in-out hover:scale-[1.1]"
+              src={collection.productImage}
+              alt={collection.productLabel}
+            />
+          ))}
         </div>
       </div>
     </section>
